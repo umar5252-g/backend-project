afc57d84522a1d822a67a1a7c152e977aa49fccd
umar5252-g <mromertariq@gmail.com>
2026-02-12 18:38:21 +0500

    created async handler util using try catch

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,18 @@
+// const asyncHandler = () => {};
+
+export { asyncHandler };
+
+// const asyncHandler = ()=>{}
+// const asyncHandler = (func)=>()=>{}
+// const asyncHandler = (func)=>async ()=>{}
+
+const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res.status(error.code || 500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
