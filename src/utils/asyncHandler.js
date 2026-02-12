068875371985a97a4300cc63746d0fc694c032c9
umar5252-g <mromertariq@gmail.com>
2026-02-12 19:20:12 +0500

    utils code discussion

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,4 +1,8 @@
-// const asyncHandler = () => {};
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
 
 export { asyncHandler };
 
@@ -6,13 +10,13 @@ export { asyncHandler };
 // const asyncHandler = (func)=>()=>{}
 // const asyncHandler = (func)=>async ()=>{}
 
-const asyncHandler = (fn) => async (req, res, next) => {
-  try {
-    await fn(req, res, next);
-  } catch (error) {
-    res.status(error.code || 500).json({
-      success: false,
-      message: error.message,
-    });
-  }
-};
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
