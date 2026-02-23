a98313447df6749c1b0b0ffaa93883628d44242b
umar5252-g <mromertariq@gmail.com>
2026-02-23 14:47:44 +0500

    fix bugs in user controler

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -24,7 +24,7 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new apiError(400, " All fields are required");
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
   if (existedUser) {
--- src/index.js
+++ src/index.js
@@ -4,21 +4,21 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 dotenv.config({ path: "./.env" });
+let password = "YUF6B2oeQFaZCjS0";
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error: ", error);
+      throw error;
+    });
 
-// connectDB()
-//   .then(() => {
-//     app.on("error", (error) => {
-//       console.log("Error: ", error);
-//       throw error;
-//     });
-
-app.listen(process.env.PORT || 8000, () => {
-  console.log(`server is running at port: ${process.env.PORT}`);
-});
-//   })
-//   .catch((error) => {
-//     console.log("MongoDB connection failed:", error);
-//   });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed:", error);
+  });
 
 // (async () => {
 //   try {
