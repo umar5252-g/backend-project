cecf8f79a507434f726c8104e24dd89928e6fcce
umar5252-g <mromertariq@gmail.com>
2026-03-03 14:38:16 +0500

    fix a bug

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -114,12 +114,12 @@ const loginUser = asyncHandler(async (req, res) => {
 
   const { username, email, password } = req.body;
 
-  if (!username || !email) {
+  if (!(username || email)) {
     throw new ApiError(400, "username or email is required");
   }
 
   const user = await User.findOne({
-    $or: [{ username, email }],
+    $or: [{ username }, { email }],
   });
 
   if (!user) {
