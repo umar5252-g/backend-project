e250588b48332da134c80d7b9b7309037b1edfaf
umarKhan <mromertariq@gmail.com>
2026-07-31 11:47:04 +0500

    implement getAllVideos controller

---
+++ src/controllers/like.contoller.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+import { Like } from "../models/like.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
