e8d7738fad9b659d432952a979d657830d51df93
umarKhan <mromertariq@gmail.com>
2026-07-31 12:07:18 +0500

    create get video controller

--- src/controllers/video.contoller.js
+++ src/controllers/video.contoller.js
@@ -54,6 +54,23 @@ const publishAVideo = asyncHandler(async (req, res) => {
 const getVideoById = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   //TODO: get video by id
+
+  if (!videoId) {
+    throw new ApiError(400, "Video id is required");
+  }
+
+  if (!mongoose.Types.ObjectId.isValid(videoId)) {
+    throw new ApiError(400, "Invalid video id");
+  }
+
+  const video = await Video.findById(videoId).populate("owner", "name email");
+  if (!video) {
+    throw new ApiError(404, "Video not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, video, "Video fetched successfully"));
 });
 
 const updateVideo = asyncHandler(async (req, res) => {
