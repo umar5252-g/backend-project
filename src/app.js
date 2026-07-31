3c65db6a70d330138726870496c26140250b9523
umarKhan <mromertariq@gmail.com>
2026-07-31 11:28:00 +0500

    mount the comment router

--- src/app.js
+++ src/app.js
@@ -18,9 +18,11 @@ app.use(cookieParser());
 
 //routes import
 import userRouter from "./routes/user.routes.js";
+import commentRouter from "./routes/comment.routes.js";
 
 //routes declaration
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/comments", commentRouter);
 
 //http://localhost:8000/api/v1/users/register
 
