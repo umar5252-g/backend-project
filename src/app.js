c643fdcd3488ede864b253f0171b1c2e95ca2a59
umarKhan <mromertariq@gmail.com>
2026-08-02 19:52:00 +0500

    mount video router

--- src/app.js
+++ src/app.js
@@ -19,10 +19,12 @@ app.use(cookieParser());
 //routes import
 import userRouter from "./routes/user.routes.js";
 import commentRouter from "./routes/comment.routes.js";
+import videoRouter from "./routes/video.routes.js";
 
 //routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/videos", videoRouter);
 
 //http://localhost:8000/api/v1/users/register
 
