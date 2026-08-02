893998dde0ca6661d95671b62cc787e87132bab6
umarKhan <mromertariq@gmail.com>
2026-08-02 19:50:46 +0500

    create video routers

---
+++ src/routes/video.routes.js
@@ -0,0 +1,22 @@
+import Router from "express";
+import {
+  getAllVideos,
+  publishAVideo,
+  getVideoById,
+  updateVideo,
+  deleteVideo,
+  togglePublishStatus,
+} from "../controllers/video.controller.js";
+
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router.route("/").get(getAllVideos);
+router.route("/publish").post(verifyJWT, publishAVideo);
+router.route("/:videoId").get(getVideoById);
+router.route("/:videoId").patch(verifyJWT, updateVideo);
+router.route("/:videoId").delete(verifyJWT, deleteVideo);
+router.route("/:videoId/toggle-publish").patch(verifyJWT, togglePublishStatus);
+
+export default router;
