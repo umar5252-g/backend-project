34ae534cada752b3d8d22b343f71fe6ef6caa535
umarKhan <mromertariq@gmail.com>
2026-08-05 00:11:03 +0500

    implement playlist routes

--- src/routes/playlist.conroller.js
+++ src/routes/playlist.conroller.js
@@ -15,3 +15,11 @@ const router = Router();
 router.route("/").post(verifyJWT, createPlaylist);
 router.route("/user/:userId").get(verifyJWT, getUserPlaylists);
 router.route("/:playlistId").get(verifyJWT, getPlaylistById);
+router.route("/:playlistId").put(verifyJWT, updatePlaylist);
+router.route("/:playlistId").delete(verifyJWT, deletePlaylist);
+router
+  .route("/:playlistId/video/:videoId")
+  .delete(verifyJWT, removeVideoFromPlaylist);
+router.route("/:playlistId/video/:videoId").post(verifyJWT, addVideoToPlaylist);
+
+export default router;
