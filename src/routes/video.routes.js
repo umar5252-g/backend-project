1b5ca71cb563de78236b0a6a4765f9986331dc79
umarKhan <mromertariq@gmail.com>
2026-08-15 09:25:19 +0500

    fix a bug in video routes

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -9,6 +9,7 @@ import {
 } from "../controllers/video.controller.js";
 
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
