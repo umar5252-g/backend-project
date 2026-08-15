47ef332f08fdeaadd91264c0204ae506746abb4c
umarKhan <mromertariq@gmail.com>
2026-08-15 09:21:29 +0500

    apply jwt on all the tweet routes

--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -8,10 +8,11 @@ import {
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router.route("/").post(verifyJWT, createTweet);
-router.route("/user/:userId").get(verifyJWT, getUserTweets);
-router.route("/:tweetId").patch(verifyJWT, updateTweet);
-router.route("/:tweetId").delete(verifyJWT, deleteTweet);
+router.route("/").post(createTweet);
+router.route("/user/:userId").get(getUserTweets);
+router.route("/:tweetId").patch(updateTweet);
+router.route("/:tweetId").delete(deleteTweet);
 
 export default router;
