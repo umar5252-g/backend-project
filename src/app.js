f2e7d25a078d3282b3c008ffb21af0333817cdb3
umarKhan <mromertariq@gmail.com>
2026-08-15 11:19:18 +0500

    create subscription routes and like routes and mount them

--- src/app.js
+++ src/app.js
@@ -21,12 +21,16 @@ import userRouter from "./routes/user.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import videoRouter from "./routes/video.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
+import subscriptionRouter from "./models/subscription.model.js";
+import likeRouter from "./routes/like.routes.js";
 
 //routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/likes", likeRouter);
 
 //http://localhost:8000/api/v1/users/register
 
