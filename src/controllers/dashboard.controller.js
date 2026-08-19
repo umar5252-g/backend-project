aa092ead710589afb59c513858b53f107e5d4b43
umarKhan <mromertariq@gmail.com>
2026-08-19 20:08:37 +0500

    implement getChannelStates controller

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -7,7 +7,43 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
-  // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+  const userId = req.user?._id;
+  if (!userId) {
+    throw new ApiError(401, "User is not authenticated");
+  }
+
+  if (!mongoose.isValidObjectId(userId)) {
+    throw new ApiError(400, "Invalid user id");
+  }
+
+  const [videoStats] = await Video.aggregate([
+    { $match: { owner: new mongoose.Types.ObjectId(userId) } },
+    {
+      $group: {
+        _id: null,
+        totalVideos: { $sum: 1 },
+        totalVideoViews: { $sum: "$views" },
+        videoIds: { $push: "$_id" },
+      },
+    },
+  ]);
+
+  const videoIds = videoStats?.videoIds ?? [];
+  const [totalSubscribers, totalLikes] = await Promise.all([
+    Subscription.countDocuments({ channel: userId }),
+    Like.countDocuments({ video: { $in: videoIds } }),
+  ]);
+
+  const stats = {
+    totalVideoViews: videoStats?.totalVideoViews ?? 0,
+    totalSubscribers,
+    totalVideos: videoStats?.totalVideos ?? 0,
+    totalLikes,
+  };
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, stats, "Channel stats fetched successfully"));
 });
 
 const getChannelVideos = asyncHandler(async (req, res) => {
