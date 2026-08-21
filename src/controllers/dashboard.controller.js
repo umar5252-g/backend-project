a1ab6877844fbc5a26648271d671aa3eee58ce6f
umarKhan <mromertariq@gmail.com>
2026-08-21 15:10:32 +0500

    implement getChannelVideos controller

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -48,6 +48,13 @@ const getChannelStats = asyncHandler(async (req, res) => {
 
 const getChannelVideos = asyncHandler(async (req, res) => {
   // TODO: Get all the videos uploaded by the channel
+
+  const userId = req.user?._id;
+  const videos = await Video.find({ owner: userId }).sort({ createdAt: -1 });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, videos, "Channel videos fetched successfully"));
 });
 
 export { getChannelStats, getChannelVideos };
